805dcc888b734c2e90a804adfc4d986f55b21400
Mark Rieke <markjrieke@gmail.com>
2021-12-24 09:16:11 -0600

    add script `update_readme()`

---
+++ scripts/update_readme.R
@@ -0,0 +1,17 @@
+# update generic ballot
+message("Updating Generic Ballot")
+messate("...")
+tictoc::tic()
+source("scripts/generic_ballot_weighting.R")
+tictoc::toc()
+message("Generic Ballot updated")
+message()
+
+# updated presidential approval
+message("Updating Presidential Approval")
+message("...")
+tictoc::tic()
+source("scripts/approval_trends_weighting.R")
+tictoc::toc()
+message("Presidential Approval updated")
+message()
