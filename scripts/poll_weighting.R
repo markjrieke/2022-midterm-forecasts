ef98e232247a10ebb94e6986514be5ca00576ab9
Mark Rieke <markjrieke@gmail.com>
2022-01-18 22:49:00 -0600

    add round 5

--- scripts/poll_weighting.R
+++ scripts/poll_weighting.R
@@ -1695,6 +1695,15 @@ if (completed == FALSE) {
   current_results %>% visualize_facet_fit()
   rmse_tracker %>% visualize_rmse()
   
+  # round 5
+  update_all()
+  
+  # round 5 viz
+  current_results <- get_current_fit()
+  current_results %>% visualize_current_fit()
+  current_results %>% visualize_facet_fit()
+  rmse_tracker %>% visualize_rmse()
+  
 }
 
 #################### TESTING ZONG MY GUY ####################
