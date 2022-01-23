628cccd3f8b2154d3c01a41ff15414f571170fe1
Mark Rieke <markjrieke@gmail.com>
2022-01-22 18:51:53 -0600

    add round 9

--- scripts/poll_weighting.R
+++ scripts/poll_weighting.R
@@ -1815,6 +1815,15 @@ if (completed == FALSE) {
   current_results %>% visualize_facet_fit()
   rmse_tracker %>% visualize_rmse()
   
+  # round 9
+  update_all()
+  
+  # round 9 viz
+  current_results <- get_current_fit()
+  current_results %>% visualize_current_fit()
+  current_results %>% visualize_facet_fit()
+  rmse_tracker %>% visualize_rmse()
+  
 }
 
 #################### EXPLORE FIT ####################
